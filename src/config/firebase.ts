86bb1ce0bd115ee84a1762aaa3092a597db80d0e
Oscar Soza <osoza@trustit.cl>
2025-09-14 03:34:19 -0300

    Otra mas

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,10 +2,13 @@ import admin from 'firebase-admin';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
-const serviceAccount = require('./firebase-service-account.json');
+// const serviceAccount = require('./firebase-service-account.json');
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || '{}');
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
+    // credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
+    credential: admin.credential.cert(serviceAccount)
 });
 
 export const db = admin.firestore();
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import * as functionsFramework from '@google-cloud/functions-framework';
 import express from 'express';
 import cors from 'cors';
 
@@ -6,15 +5,10 @@ import { PORT } from './config/env.js';
 import { corsOptions } from './config/cors.js';
 import apiRoutes from './interfaces/http/routes/routes.js';
 
-import './config/firebase.js';
-
 const app = express();
 app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/api', apiRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
-// app.listen(PORT, () => console.log(`Servidor escuchando en http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Servidor escuchando en http://localhost:${PORT}`));
