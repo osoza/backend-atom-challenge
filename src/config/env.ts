31f63db4e7be84e0207f47544885b27235108058
Oscar Soza <osoza@trustit.cl>
2025-09-14 19:38:05 -0300

    Verificación error de CORS

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,4 +3,5 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 8080;
 export const JWT_SECRET = process.env.JWT_SECRET || 'secret';
-export const CORS_ORIGIN = process.env.CORS_ORIGIN || '*';
+export const CORS_ORIGIN = process.env.CORS_ORIGIN || 
+  'http://localhost:4200,http://localhost:5000,https://atom-challenge-frontend.web.app';
