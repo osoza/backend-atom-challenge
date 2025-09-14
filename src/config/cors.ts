860ca187fe960185e13b3134992aee4652477e5b
Oscar Soza <osoza@trustit.cl>
2025-09-14 19:53:09 -0300

    Todo funcionando

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,8 +1,22 @@
 import cors from 'cors';
 import { CORS_ORIGIN } from './env.js';
 
+const allowedOrigins = [
+   'http://localhost:4200',
+   'http://localhost:5000',
+   'https://atom-challenge-frontend.web.app'
+];
+
 export const corsOptions = {
-   origin: CORS_ORIGIN,
+   origin: function (origin: string | undefined, callback: Function) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) !== -1) {
+         callback(null, true);
+      } else {
+         callback(new Error('OSN Not allowed by CORS: ' + origin));
+      }
+   },
    methods: ['GET', 'POST', 'PUT', 'DELETE'],
    allowedHeaders: ['Content-Type', 'Authorization']
 };
